03dd93ea6efd2d382b106406ca56df33e40c36d5
Caden Brooks <caden@brooks.nu>
2025-03-03 17:04:03 -0700

    first commit

---
+++ gui.py
@@ -0,0 +1,6 @@
+import tkinter
+
+
+class GUI():
+    def __init__(self):   # treate this as the main
+        pass
---
+++ main.py
@@ -0,0 +1,10 @@
+from gui import GUI
+from algorithm_bot import Bot
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
